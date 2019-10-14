6742366534881184013a6cb8ffa65eeb67c8b1f6
Thomas Banks <thomas.banks@havaslynx.com>
2019-10-14 19:29:01 +0100

    Configured the site to read and publish markdown blog posts

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
 import resolve from 'rollup-plugin-node-resolve';
 import replace from 'rollup-plugin-replace';
-import commonjs from 'rollup-plugin-commonjs';
 import svelte from 'rollup-plugin-svelte';
-import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
 import pkg from './package.json';
@@ -19,6 +20,7 @@ export default {
 		input: config.client.input(),
 		output: config.client.output(),
 		plugins: [
+      json(),
 			replace({
 				'process.browser': true,
 				'process.env.NODE_ENV': JSON.stringify(mode)
